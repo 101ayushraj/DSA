c7814b07acdf9c104b33aa1b857d34f2e3cbd26f
101ayushraj <404ayushraj@gmail.com>
2026-07-19 12:29:57 +0530

    Time: 102 ms (5.02%) | Memory: 54.5 MB (6.43%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,11 +1,22 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        seen,repeated=set(),set()
+        ans=[]
+        tracker = defaultdict(lambda: defaultdict(int))
         m=len(s)
-        for i in range(0,m-9):
-            t=s[i:i+10]
-            if t in seen:
-                repeated.add(t)
-            else:
-                seen.add(t)
-        return list(repeated)
+        if m<=10:
+            return ans
+        base=4
+        highest_power=pow(4,9)
+        text_hash=0
+        for i in range(10):
+            text_hash=(text_hash*base)+ord(s[i])
+        pattern_hash=text_hash
+        tracker[pattern_hash][s[0:10]] = 1
+        for i in range(1,m-9):
+            pattern_hash=(pattern_hash-ord(s[i-1])*highest_power)*base + ord(s[i+9])
+            tracker[pattern_hash][s[i:i+10]] += 1
+        for pattern_hash, string_counts in tracker.items():
+            for unique_string, count in string_counts.items():
+                if count > 1:
+                    ans.append(unique_string)
+        return ans
