8a9a54cd0beb5cdee15f833e883f5363bc755f0f
101ayushraj <404ayushraj@gmail.com>
2026-07-19 12:24:49 +0530

    Time: 26 ms (58.88%) | Memory: 30.8 MB (74.99%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,14 +1,11 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        ans=[]
-        slider=defaultdict(int)
+        seen,repeated=set(),set()
         m=len(s)
-        old_word=s[:10]
-        slider[old_word]=1
-        for i in range(1,m-9):
-            old_word=old_word[1:]+s[i+9]
-            slider[old_word]+=1
-        for key,value in slider.items():
-            if value>1:
-                ans.append(key)
-        return ans
+        for i in range(0,m-9):
+            t=s[i:i+10]
+            if t in seen:
+                repeated.add(t)
+            else:
+                seen.add(t)
+        return list(repeated)
